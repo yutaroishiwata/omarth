8c1a7122ac0582e4b549031c1fba74065ea554b6
yutaroo <another.sky1219@gmail.com>
2019-01-29 02:13:27 +0900

    add fix

--- app/js/sketch.js
+++ app/js/sketch.js
@@ -1,11 +1,11 @@
 // https://kylemcdonald.github.io/cv-examples/
 // https://github.com/kylemcdonald/AppropriatingNewTechnologies/wiki/Week-2
 
-var capture;
 var tracker;
 var w = 400,
     h = 400;
-var pg; //キャンバス
+var canvas;
+var pg; //特徴量用キャンバス
 var angle = 0;  // 角度
 
 function setup() {
@@ -26,6 +26,7 @@ function setup() {
 
   colorMode(HSB);
   stroke(0);
+  frameRate(14);
 
   tracker = new clm.tracker();
   tracker.init();
@@ -35,15 +36,17 @@ function setup() {
 function draw() {
   translate(width/2, height/2);
   background(255);
-  pg.arc(w/-2, h/-2, w, h, Math.PI*2, false);
+
   image(pg, w/-2, h/-2, w, h); //createGraphics中央配置
-  pg.background(51);
+  //image(capture, w/-2, h/-2, w, h);
+  pg.background(255);
   var positions = tracker.getCurrentPosition();
 
   pg.noStroke();
+
   //特徴点の座標にポイントと番号を配置
   for (var i = 0; i < positions.length; i++) {
-    pg.fill(255);
+    pg.fill(51);
     pg.ellipse(positions[i][0], positions[i][1], 2, 2);
 
     //座標を62番を基準に再計算
@@ -63,18 +66,20 @@ function draw() {
     }
 
     arrayRecalc[i] = [Math.round(recalc0), Math.round(recalc1)];
-    //console.log(arrayRecalc);
+    console.log(arrayRecalc);
   }
 
-  linesArcCW(200, 10, 5);
-  linesArcCCW(200, 40, 2);
+  //アート生成関数
+  linesArcCW(200, recalc0, 5 * recalc0);
+  linesArcCCW(200, recalc1, 5 * recalc1);
 }
 
+//時計回り
 function linesArcCW(r, l, s) {
   push();
   var r; //半径
   var l; //線の本数
-  var s; //線の描画
+  var s; //線の描画間隔
   for(var i = 0; i < l; i++) {
     var sX = sin(radians(angle + (i * s))) * r; //始点X座標
     var sY = cos(radians(angle + (i * s))) * -r;　//終点Y座標
@@ -86,6 +91,7 @@ function linesArcCW(r, l, s) {
   angle += 2;　//回転速度
 }
 
+//半時計回り
 function linesArcCCW(r, l, s) {
   push();
   var r; //半径
